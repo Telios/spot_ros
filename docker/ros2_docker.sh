1926f5b751d99907ca9c62e7b7492d5af7f64a01
Telios <e11809916@student.tuwien.ac.at>
2023-04-05 10:49:06 -0400

    add ros2 dockerfile

---
+++ docker/ros2_docker.sh
@@ -0,0 +1,18 @@
+xhost local:root
+
+XAUTH=/tmp/.docker.xauth
+
+docker run -it \
+    --name=spot_driver \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    --net=host \
+    --privileged \
+    --rm \
+    spot_ros2 \
+    bash
+
+echo "Done."
